5537882627c62ef0516e8d902c34748535c1df3e
skille97 <laurits.randers@gmail.com>
2018-12-23 18:10:27 +0100

    dockomentasion

--- src/eksamne.cpp
+++ src/eksamne.cpp
@@ -1,3 +1,11 @@
+/*
+Laurits Randers imprative programmings projekt
+
+projektet er en variation af projekt forslag 2 – måling af reaktionstid
+
+Der er implementeret et menu system, hvor man kan måle reaktionstid på en spiller eller 5 spillere. Scoren kan herefter printes ud og evt resettes til næste gang.
+*/
+
 #include <Arduino.h>
 
 struct player{
@@ -17,13 +25,17 @@ int menuID = 1;
 int lastMenuID = 0;
 
 void waitRandomTime(){
+  // Wait random time btween 500 and 2000 ms
   delay(random(500, 2000));
 }
+
 void waitUntilKeyPressed(){
+  // Wait until Key 2 is pressed
   while(digitalRead(2)){}
 }
 
 int measuresReactionTime(){
+  // Measure reaction time and retunes it
   unsigned long startTime = 0;
   unsigned long stopTime = 0;
   digitalWrite(LED_BUILTIN, LOW);
@@ -36,25 +48,33 @@ int measuresReactionTime(){
 }
 
 void printReactionTime(int reactionTime){
+  // Prints reaction time to serial port
+
   Serial.print("your reaction time is ");
   Serial.print(reactionTime);
   Serial.println(" milliseconds");
 }
 
 void singleplayer(){
+  // Measures reaction time and prints it out for single player
+  // Waits 1000 ms before returning
+
   int reactionTime = measuresReactionTime();
   printReactionTime(reactionTime);
-  delay(500);
+  delay(1000);
 }
 
 void definePlayerArray(player playerArray[]) {
-  for(int i = 0; i != numbersOfPlayer; i++){
+  // Fill in names in playerArray
+
+  for(int i = 0; i < numbersOfPlayer; i++){
     sprintf(playerArray[i].name, "player %d", i);
   }
 }
 
 void printPlayers(player playerArray[]) {
-  for(int i = 0; i != numbersOfPlayer; i++){
+  // Prints player name and average reaction time on serial port
+  for(int i = 0; i < numbersOfPlayer; i++){
     Serial.print(playerArray[i].name);
     Serial.print(" average reaction times is ");
     Serial.println(playerArray[i].averageReactionTimes);
@@ -62,7 +82,8 @@ void printPlayers(player playerArray[]) {
 }
 
 void ReactionTimeMultipleTimes(struct player *player){
-  for(int i = 0; i != 5; i++){
+  // Measure reaction time 5 times
+  for(int i = 0; i < 5; i++){
     Serial.print("Test ");
     Serial.println(i + 1);
     player->reactionTimes[i] = measuresReactionTime();
@@ -70,9 +91,10 @@ void ReactionTimeMultipleTimes(struct player *player){
 }
 
 void calculateaverage(player playerArray[]){
-  for(int i = 0; i != numbersOfPlayer; i++){
+  // Calculate average reaction time for all players
+  for(int i = 0; i < numbersOfPlayer; i++){
     int sum = 0;
-    for(int j = 0; j != 5; j++){
+    for(int j = 0; j < 5; j++){
       sum += playerArray[i].reactionTimes[j];
     }
     playerArray[i].averageReactionTimes = sum/5;
@@ -80,10 +102,11 @@ void calculateaverage(player playerArray[]){
 }
 
 void multiplayer() {
+  // Measure reaction time for 5 players
   Serial.println();
   Serial.println("you have selected multiplayer");
   definePlayerArray(playerArray);
-  for(int i = 0; i != 5; i++){
+  for(int i = 0; i < numbersOfPlayer; i++){
     delay(500);
     Serial.print("it is ");
     Serial.print(playerArray[i].name);
@@ -92,23 +115,26 @@ void multiplayer() {
     waitUntilKeyPressed();
     ReactionTimeMultipleTimes(&playerArray[i]);
   }
-  Serial.println("all the Test are done");
+  Serial.println("all the test are done");
   calculateaverage(playerArray);
   delay(500);
 }
 
 void scores(){
+  // Prints out reaction time for all players
   printPlayers(playerArray);
 }
 
 void reset() {
+  // Empties PlayerArray filling it with Null
   memset(playerArray, 0, sizeof(playerArray));
 }
 
 void printMenu(int menuID){
-
+  // Prints out menu items once
+  // When key is pressed the function is called
   switch(menuID){
-    case 1:
+    case 1: // Single player
       if(menuID != lastMenuID){
         Serial.println(">Single player");
       }
@@ -117,7 +143,7 @@ void printMenu(int menuID){
       }
       break;
 
-    case 2:
+    case 2: // Multiplayer
       if(menuID != lastMenuID){
         Serial.println(">Multiplayer");
       }
@@ -126,7 +152,7 @@ void printMenu(int menuID){
       }
       break;
 
-    case 3:
+    case 3: // Scores
       if(menuID != lastMenuID){
         Serial.println(">Scores");
       }
@@ -135,9 +161,9 @@ void printMenu(int menuID){
       }
       break;
 
-    case 4:
+    case 4: // Reset scores
       if(menuID != lastMenuID){
-        Serial.println(">reset Scores");
+        Serial.println(">Reset scores");
         }
       if(!digitalRead(2)){
         reset();
@@ -148,6 +174,8 @@ void printMenu(int menuID){
 }
 
 void menu(){
+  // If Down button is pressed the menu ID is decreased and wrapped to 4 when decreased to 1
+  // If Up button is pressed the menu ID is increased and wrapped to 4 when increased to 5
   if(!digitalRead(butDown)){
     if(menuID <= 1){
       menuID = 4;
@@ -164,13 +192,14 @@ void menu(){
       menuID++;
     }
   }
-  //Serial.println(menuID);
+
   printMenu(menuID);
   delay(250);
 }
 
 
 void setup() {
+  // Defines pinModes of the IO and initialises serial communication
   pinMode(2, INPUT_PULLUP);
   pinMode(butUp, INPUT_PULLUP);
   pinMode(butDown, INPUT_PULLUP);
@@ -183,15 +212,6 @@ void setup() {
 }
 
 void loop() {
+  // calls menu function
   menu();
 }
-
-
-
-
-void turnOnLED();
-void registerStartTime();
-void waitUntilKeyPressed();
-void registerStopTime();
-void calcReactionTime();
-void presentResult();
